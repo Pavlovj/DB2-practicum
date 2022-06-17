bae4c359d5d7b0f370f11c0d42cd7dab4872d5b0
Jesaja Pavlović <jesaja.pavlovic@hva.nl>
2022-06-17 19:35:10 +0200

    crud reizigers nosql

--- src/nl/hva/ict/controllers/ReizigerController.java
+++ src/nl/hva/ict/controllers/ReizigerController.java
@@ -7,6 +7,8 @@ import nl.hva.ict.models.Reiziger;
 import nl.hva.ict.views.ReizigersView;
 import nl.hva.ict.views.View;
 
+import java.util.HashMap;
+
 /**
  * ReizigerController
  * @author HBO-ICT
@@ -35,8 +37,10 @@ public class ReizigerController extends Controller {
         // voor NOSQL
         ObservableList<Reiziger> reizigers = FXCollections.observableArrayList(MainApplication.getMongoDBReizigers().getAll());
 
+
         reizigersView.getReizigersViewListView().setItems(reizigers);
         reizigersView.getComboReistSamenMet().getSelectionModel().select(null);
+
     }
 
 
@@ -45,15 +49,44 @@ public class ReizigerController extends Controller {
     }
 
     private void save() {
-        // bewaar (update) record
+        // pas1 dit record aan
+        HashMap<String, String> reizerObj = new HashMap<>();
+        reizerObj.put("oldReizigersCode", reizigersView.getReizigersViewListView().getSelectionModel().getSelectedItem().getReizigersCode());
+        reizerObj.put("newReizigersCode", reizigersView.getTxtReizigersCode().getText());
+        reizerObj.put("voornaam", reizigersView.getTxtVoornaam().getText());
+        reizerObj.put("achternaam", reizigersView.getTxtAchternaam().getText());
+        reizerObj.put("adres", reizigersView.getTxtAdres().getText());
+        reizerObj.put("postcode", reizigersView.getTxtPostcode().getText());
+        reizerObj.put("plaats", reizigersView.getTxtPlaats().getText());
+        reizerObj.put("land", reizigersView.getTxtLand().getText());
+        reizerObj.put("gezinshoofd", reizigersView.getComboReistSamenMet().getSelectionModel().getSelectedItem().getReizigersCode());
+
+        MainApplication.getMongoDBReizigers().update(reizerObj);
+        reizigersView.getReizigersViewListView().setItems(FXCollections.observableArrayList(MainApplication.getMongoDBReizigers().getAll()));
+
     }
 
     private void delete() {
         // delete dit record
+        MainApplication.getMongoDBReizigers().remove(reizigersView.getReizigersViewListView().getSelectionModel().getSelectedItem());
+        reizigersView.getReizigersViewListView().setItems(FXCollections.observableArrayList(MainApplication.getMongoDBReizigers().getAll()));
     }
 
     private void insert() {
         //Voeg toe
+        HashMap<String, String> reizigerObj = new HashMap<>();
+        reizigerObj.put("reiziger_code", reizigersView.getTxtReizigersCode().getText());
+        reizigerObj.put("voornaam", reizigersView.getTxtVoornaam().getText());
+        reizigerObj.put("achternaam", reizigersView.getTxtAchternaam().getText());
+        reizigerObj.put("adres", reizigersView.getTxtAdres().getText());
+        reizigerObj.put("postcode", reizigersView.getTxtPostcode().getText());
+        reizigerObj.put("plaats", reizigersView.getTxtPlaats().getText());
+        reizigerObj.put("land", reizigersView.getTxtLand().getText());
+        reizigerObj.put("gezinshoofd", reizigersView.getComboReistSamenMet().getSelectionModel().getSelectedItem().getReizigersCode());
+
+        MainApplication.getMongoDBReizigers().add(reizigerObj);
+        reizigersView.getReizigersViewListView().setItems(FXCollections.observableArrayList(MainApplication.getMongoDBReizigers().getAll()));
+
     }
 
     private void getItemsInFields() {
--- src/nl/hva/ict/data/MongoDB/MongoReizigers.java
+++ src/nl/hva/ict/data/MongoDB/MongoReizigers.java
@@ -1,10 +1,18 @@
 package nl.hva.ict.data.MongoDB;
 
 import com.mongodb.client.MongoCursor;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.UpdateResult;
 import nl.hva.ict.MainApplication;
 import nl.hva.ict.models.Reiziger;
 import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -50,6 +58,21 @@ public class MongoReizigers extends MongoDB {
     @Override
     public void add(Object object) {
 
+        if (object instanceof HashMap<?, ?>) {
+            HashMap<String, String> reizigerObj = (HashMap<String, String>) object;
+            collection.insertOne(new Document()
+                    .append("_id", new ObjectId())
+                    .append("reiziger_code", reizigerObj.get("reiziger_code"))
+                    .append("voornaam", reizigerObj.get("voornaam"))
+                    .append("achternaam", reizigerObj.get("achternaam"))
+                    .append("adres", reizigerObj.get("adres"))
+                    .append("postcode", reizigerObj.get("postcode"))
+                    .append("plaats", reizigerObj.get("plaats"))
+                    .append("land", reizigerObj.get("land"))
+                    .append("gezinshoofd", reizigerObj.get("gezinshoofd"))
+            );
+            load();
+        }
     }
 
 
@@ -58,7 +81,23 @@ public class MongoReizigers extends MongoDB {
      */
     @Override
     public void update(Object object) {
+        if (object instanceof HashMap<?, ?>) {
+            HashMap<String, String> reizigerObj = (HashMap<String, String>) object;
+            Bson filter = Filters.eq("reiziger_code", reizigerObj.get("oldReizigersCode"));
+            Bson update = Updates.combine(
+                    Updates.set("reiziger_code", reizigerObj.get("newReizigersCode")),
+                    Updates.set("voornaam", reizigerObj.get("voornaam")),
+                    Updates.set("achternaam", reizigerObj.get("achternaam")),
+                    Updates.set("adres", reizigerObj.get("adres")),
+                    Updates.set("postcode", reizigerObj.get("postcode")),
+                    Updates.set("plaats", reizigerObj.get("plaats")),
+                    Updates.set("land", reizigerObj.get("land")),
+                    Updates.set("gezinshoofd", reizigerObj.get("gezinshoofd"))
+            );
 
+            UpdateResult result = collection.updateOne(filter, update);
+            load();
+        }
     }
 
     /**
@@ -66,6 +105,11 @@ public class MongoReizigers extends MongoDB {
      */
     @Override
     public void remove(Object object) {
+        if (object instanceof Reiziger) {
+            Bson filter = Filters.eq("reiziger_code", ((Reiziger) object).getReizigersCode());
+            DeleteResult result = collection.deleteOne(filter);
+            load();
+        }
     }
 
     /**
